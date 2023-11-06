00f6d19594d715e6be31ce1aa6b378bd9da4fbff
benji <tbkertelt@outlook.com>
2023-11-06 10:39:17 -0300

    ## created

---
+++ main.js
@@ -0,0 +1,25 @@
+const form = document.getElementById('form-atividade')
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
+
+    const inputNomeAtividade = document.getElementById('nome-atividade')
+    const inputNotaAtividade = document.getElementById('nota-atividade')
+
+    // ADICIONA AS INFORMAÇÕES DE ATIVIDADE, NOTA E SE O ALUNO FOI APROVADO OU NÃO
+    let linha = '<tr>';
+    //cria a variável LINHA, que recebe o conteúdo da string html "<tr>"
+    linha += `<td>${inputNomeAtividade.value}</td>`;
+    // concatenação:   linha = linha + 'outro conteudo
+    linha += `<td>${inputNotaAtividade.value}</td>`;
+    linha += `<td>${inputNotaAtividade.value >= 7 ? 'Aprovado' : 'Reprovado'}</td>`;
+    //if positivo = ? / else = : 
+    linha = '</tr>';
+
+    // COLOCA OS DADOS NA TABELA
+    const corpoTabela = document.querySelector('tbody');
+    // retorna o primeiro item do CSS correspondente
+    corpoTabela.innerHTML = linha;
+    // insere o conteúdo dentro da tag
+
+    
+});
