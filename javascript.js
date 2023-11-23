ae67c55f2053d3d158f1cb1dc1763efa7ef98ff4
benji <tbkertelt@outlook.com>
2023-11-23 10:29:37 -0300

    ### Added javascript.js

---
+++ javascript.js
@@ -0,0 +1,38 @@
+$(document).ready(function () {
+
+    $('#telefone').mask('(00) 00000-0000');
+    $('#cpf').mask('000.000.000-00');
+    $('#cep').mask('00000-000');
+
+    $('form').validate({
+        rules: {
+            nome: {
+                required: true
+            },
+            telefone: {
+                required: true
+            },
+            email: {
+                required: true,
+                email: true
+            },
+            cpf: {
+                required: true
+            }
+        },
+        messages: {
+            nome: 'Insira seu nome completo'
+        },
+        submitHandler: function (form) {
+            console.log(form)
+        },
+        invalidHandler: function (evento, validador) {
+            let camposIncorretos = validador.numberOfInvalids();
+            if (camposIncorretos) {
+                alert(`Existem ${camposIncorretos} campos incorretos.`)
+            }
+        }
+    })
+
+
+});
