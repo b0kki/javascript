7801038efbfdef9ae3e7166bb50b3debd655b7f3
benji <tbkertelt@outlook.com>
2024-01-19 10:27:57 -0300

    Added gruntfile.js

---
+++ gruntfile.js
@@ -0,0 +1,33 @@
+module.exports = function (grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        less: {
+            development: {
+                files: {
+                    './build/styles/main.css': './src/styles/main.less'
+                }
+            },
+            production: {
+                options: {
+                    compress: true,
+                },
+                files: {
+                    './build/styles/main.min.css': './src/styles/main.less'
+                }
+            }
+        },
+        uglify: {
+            target: {
+                files: {
+                    'build/scripts/main.min.js': 'src/scripts/main.js'
+                }
+            }
+        }
+    })
+
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.registerTask('default', ['less', 'uglify']);
+
+
+}
