74ab6b411314e117036531351097d236b09c0fb4
benji <tbkertelt@outlook.com>
2024-02-21 08:33:41 -0300

    initial building, added section countdown

---
+++ src/scripts/main.js
@@ -0,0 +1,28 @@
+AOS.init();
+
+const dataEvento = new Date("Apr 13, 2024 00:00:00");
+const timeStamp = dataEvento.getTime();
+
+const timeCount = setInterval(function () {
+    const now = new Date();
+    const timeStampNow = now.getTime();
+
+    const eventDay = timeStamp - timeStampNow;
+
+    const dayMS = 1000 * 60 * 60 * 24;
+    const hourMS = 1000 * 60 * 60;
+    const minMS = 1000 * 60;
+
+    const daysToEvent = Math.floor(eventDay / dayMS)
+    const hoursToEvent = Math.floor((eventDay % dayMS) / hourMS)
+    const minsToEvent = Math.floor(eventDay % hourMS / (minMS))
+    const secToEvent = Math.floor((eventDay % minMS) / 1000);
+
+    document.getElementById("countdown").innerHTML = `${daysToEvent}d ${hoursToEvent}g ${minsToEvent}m ${secToEvent}s`
+
+    if (eventDay < 0) {
+        clearInterval(timeCount);
+        document.getElementById('countdown').innerHTML - 'Já envelheceu!'
+
+    }
+}, 1000)
