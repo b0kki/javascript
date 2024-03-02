f4386b3cf66a8fe92c87856f7d95ad48d7f68025
benji <tbkertelt@outlook.com>
2024-03-02 07:30:22 -0300

    updated

--- src/grades.js
+++ src/grades.js
@@ -1,23 +1,18 @@
-const student = ['jao', 8, 'fabio', 'jaqueline', 'marcia']
+const student = ['jao', 'fabio', 'jaqueline', 'marcia']
 const grades = [9, 4, 5, 8]
 
 
-const alunos = student.map(function (itemAtual) {
+const medias = student.map(function (item, index) {
     return {
-        nome: itemAtual,
-    }
-})
-
-const medias = grades.map(function (itemAtual) {
-    return {
-        nota: itemAtual,
+        nome: item,
+        nota: grades[index]
     }
 })
 
 
-
-const all = [...grades].filter(function(item) {
-    return item >= 6;
+const result = grades.filter(function (nota) {
+    return nota >= 6;
 })
 
-console.log(`Os seguintes alunos estão dentro/acima da média: ${all}`)
+
+console.log(`Os seguintes alunos estão dentro/acima da média: ${result}`)
