4411fe81b74f6b72d5c6d9c335be496667af3ecb
benji <tbkertelt@outlook.com>
2024-03-14 12:05:02 -0300

    added styles.css

--- src/components/Formulario/index.jsx
+++ src/components/Formulario/index.jsx
@@ -1,16 +1,25 @@
-import { useState } from "react"
+import { useState, useEffect } from "react";
+import './styles.css';
 
 const Formulario = () => {
     const [weight, setWeight] = useState(0);
     const [height, setHeight] = useState(0);
 
-    const imc = () => {
-        const square = height * height
-        const result = weight / square
 
-        if (result <= 19) {
+
+    useEffect(() => {
+    }, [weight, height]);
+
+    const imc = () => {
+        const result = weight / (height * height)
+
+        if (result <= 18.5) {
             return (
                 <p>Seu IMC é {result}. Você está abaixo do peso.</p>
+            )
+        } else if (result >= 18.5 && imc <= 25) {
+            return (
+                <p>Seu IMC é {result}. Você está no peso ideal.</p>
             )
         } else {
             return (
@@ -21,8 +30,8 @@ const Formulario = () => {
 
     return (
         <div className="container">
-            <h1>Calcule seu IMC</h1>
             <form>
+                <h1>Calcule seu IMC</h1>
                 <input type="number" placeholder="Digite seu peso" onChange={({ target }) => setWeight(parseInt(target.value))} />
                 <input type="number" placeholder="Digite sua altura" onChange={({ target }) => setHeight(parseInt(target.value))} />
                 {imc()}
