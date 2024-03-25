e38ef0f56a7df78b0341aee64e318969b1f9b6b7
benji <tbkertelt@outlook.com>
2024-03-25 14:24:27 -0300

    fixed bugs

--- src/components/Formulario/index.jsx
+++ src/components/Formulario/index.jsx
@@ -1,48 +1,49 @@
 import { useEffect, useState } from "react";
-import './styles.css';
+import "./styles.css";
 
 const Formulario = () => {
-    const [weight, setWeight] = useState(0);
-    const [height, setHeight] = useState(0);
+  const [weight, setWeight] = useState(0);
+  const [height, setHeight] = useState(0);
+  const [result, setResult] = useState(0);
 
+  const imc = () => {
+    const result = weight / (height * height);
 
-    const imc = () => {
-        const result = weight / (height * height)
-
-        if (result <= 18.5) {
-            return (
-                <p>Seu IMC é {result}. Você está abaixo do peso.</p>
-            )
-        } else if (result >= 18.5 && imc <= 25) {
-            return (
-                <p>Seu IMC é {result}. Você está no peso ideal.</p>
-            )
-        } else {
-            return (
-                <p>Seu IMC é {result}. Você está acima do peso.</p>
-            )
-        }
-
+    if (result <= 18.5) {
+      setResult(`Seu IMC é ${result}. Você está abaixo do peso`);
+    } else if (result >= 18.5 && result <= 25) {
+      setResult(`Seu IMC é ${result}. Você está no peso ideal`);
+    } else if (result >= 25 && result <= 29) {
+      setResult(`Seu IMC é ${result}. Você está com obesidade grau I`);
+    } else if (result >= 30 && result <= 39) {
+      setResult(`Seu IMC é ${result}. Você está com pbesidade grau II`);
+    } else {
+      setResult(`Seu IMC é ${result}.Você está com pbesidade grau III`);
     }
+  };
 
-    // useEffect(() => {
-    //     return (
-    //         imc()
-    //     )
-    // }, [weight, height]);
+  useEffect(() => {
+    imc();
+  }, [weight, height]);
 
-    return (
-        <div className="container">
-            <form>
-                <h1>Calcule seu IMC</h1>
-                <input type="number" placeholder="Digite seu peso" onChange={({ target }) => setWeight(parseInt(target.value))} />
-                <input type="number" placeholder="Digite sua altura" onChange={({ target }) => setHeight(parseInt(target.value))} />
-            </form>
-        </div>
-    )
-}
+  return (
+    <div className="container">
+      <form>
+        <h1>Calcule seu IMC</h1>
+        <input
+          onChange={({ target }) => setWeight(parseInt(target.value))}
+          type="number"
+          placeholder="Digite seu peso"
+        />
+        <input
+          onChange={({ target }) => setHeight(parseInt(target.value))}
+          type="number"
+          placeholder="Digite sua altura"
+        />
+      </form>
+      <h4>{result}</h4>
+    </div>
+  );
+};
 
-
-
-export default Formulario
-
+export default Formulario;
